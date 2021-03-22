232448a66843ddd3b2b768c97cdf51abe401edbc
JEAN-BAPTISTE <jgeorges24@icloud.com>
2021-03-22 18:27:12 -0400

    setting up environment folders

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+# ENV['SINATRA_ENV'] ||= "development"
+# ENV['RACK_ENV'] ||= "development"
+
+#require_relative '../controllers/application_controller.rb'
+#Bundler.require(:default, ENV['SINATRA_ENV'])
+# require_all './app'
+
+Bundler.require
+require 'bundler/setup'
+require_all './app'
